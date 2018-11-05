36d663bf09f88288accf84a1787163912e4d2844
Yama1020 <ryo.yamamoto@nttcsol.com>
2018-11-05 13:14:32 +0900

    modify test

--- application.py
+++ application.py
@@ -1,6 +1,8 @@
 from flask import Flask
+import numpy
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    test = numpy.random.rand()
+    return "Hello World! Number {}.format(test)"
