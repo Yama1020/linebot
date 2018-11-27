55e78ed30c14745b207cb8324a5443e0817f0ef9
Yama1020 <ryo.yamamoto@nttcsol.com>
2018-11-27 10:34:14 +0900

    1034

--- application.py
+++ application.py
@@ -43,7 +43,7 @@ handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
 @app.route("/")
 def hello():
-    dbq = UserList.query.all()
+    dbq = str(UserList.query.all())
     return dbq
 
 @app.route('/webhook', methods=['POST'])
