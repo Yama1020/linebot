2f6d537fba83428520ee406e8ca5e46a3cd15c15
Yama1020 <ryo.yamamoto@nttcsol.com>
2018-11-28 15:40:23 +0900

    1539

--- application.py
+++ application.py
@@ -58,7 +58,7 @@ def hello():
 # DB接続試験用(SELECT文確認)
 @app.route("/query")
 def query():
-    dbquery = db.session.query(UserList.username, UserList.userid).all()
+    dbquery = db.session.query(UserList.username).first()
     ret = str(dbquery)
     return ret
 
