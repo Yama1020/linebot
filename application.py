6b6cc5eb19b41c2e7d1a1057dda44bed0ad8cf8a
Yama1020 <ryo.yamamoto@nttcsol.com>
2018-11-28 14:48:12 +0900

    1448

--- application.py
+++ application.py
@@ -53,7 +53,7 @@ def hello():
 
 @app.route("/query")
 def query():
-    dbq = db.session.query(UserList.username).all()
+    dbq = db.session.query(UserList.username, UserList.userid).all()
     ret = str(dbq)
     return ret
 
