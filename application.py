651494f95bc402997ab1ac752879231f7203b06f
Yama1020 <ryo.yamamoto@nttcsol.com>
2018-11-29 16:39:05 +0900

    1639

--- application.py
+++ application.py
@@ -96,7 +96,7 @@ def callback():
 # LineBotにテキスト送信があった際の挙動(”こんにちは”と返すだけ)
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    repmes = "こんにちは"
+    repmes = event.message.id
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=repmes))
