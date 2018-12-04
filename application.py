8dc73fbf2380e5264a6ba36b75911a6090fef72c
Yama1020 <ryo.yamamoto@nttcsol.com>
2018-12-04 13:26:44 +0900

    1326

--- application.py
+++ application.py
@@ -114,8 +114,7 @@ def handle_imgmessage(event):
     message_content = line_bot_api.get_message_content(message_id)
 
     i = Image.open(BytesIO(message_content.content))
-    filepath = '/home/'
-    filename = filepath + message_id + '.jpg'
+    filename = '/home/' + message_id + '.jpg'
     i.save(filename)
 
     accountname = os.environ["STORAGE_NAME"]
@@ -123,7 +122,7 @@ def handle_imgmessage(event):
     block_blob_service = BlockBlobService(account_name=accountname, account_key=accountkey)
     container_name ='images'
     
-    block_blob_service.create_blob_from_path(container_name, message_id, filepath)
+    block_blob_service.create_blob_from_path(container_name, message_id, filename)
 
 # LineBotを友達追加orブロック解除した際の挙動(UserListテーブルに相手のLINEの表示名、IDを追加しつつ応答を返す)
 @handler.add(FollowEvent)
